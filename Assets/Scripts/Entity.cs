f9d2266fcd29c4d58cfeca4e9ee1c88d47cad71c
Fred Zhang <fred.zhang@skyboxlabs.com>
2026-01-31 11:26:01 -0800

    first commit

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    protected ScriptableObject data;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+}
