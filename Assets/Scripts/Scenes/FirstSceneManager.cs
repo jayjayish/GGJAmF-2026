14f1481788da66fd9cbce2240dc2605dfe9d77aa
jayjayish <42991703+jayjayish@users.noreply.github.com>
2026-01-31 11:56:12 -0800

    InputManager

---
+++ Assets/Scripts/Scenes/FirstSceneManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Scenes
+{
+    public class FirstSceneManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
