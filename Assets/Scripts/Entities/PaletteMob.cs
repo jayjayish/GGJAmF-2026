d62ad4929ca39c0422ee58b02d8ce98992dfecef
jayjayish <42991703+jayjayish@users.noreply.github.com>
2026-02-01 12:13:33 -0800

    Palette mob

---
+++ Assets/Scripts/Entities/PaletteMob.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PaletteMob : BasicMob
+{
+    []
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
